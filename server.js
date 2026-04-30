aa9ed2d986e022ff15833bbeb296096e49885427
Omar Navarro <onavarro@ingyso.com>
2026-04-29 23:36:19 -0700

    Added files using EJS templates

--- server.js
+++ server.js
@@ -11,8 +11,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const name = process.env.NAME || 'CSE 340 World';
+const NODE_ENV = process.env.NODE_ENV || 'production';
 // Define the port number the server will listen on
 const PORT = process.PORT || 3000;
+
+// When in development mode, start a WebSocket server for live reloading
+if (NODE_ENV.includes('dev')) {
+    const ws = await import('ws');
+
+    try {
+        const wsPort = parseInt(PORT) + 1;
+        const wsServer = new ws.WebSocketServer({ port: wsPort });
+
+        wsServer.on('listening', () => {
+            console.log(`WebSocket server is running on port ${wsPort}`);
+        });
+
+        wsServer.on('error', (error) => {
+            console.error('WebSocket server error:', error);
+        });
+    } catch (error) {
+        console.error('Failed to start WebSocket server:', error);
+    }
+}
 
 /**
  * Setup Express Server
@@ -27,6 +48,26 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+/**
+ * Global template variables middleware
+ * 
+ * Makes common variables available to all EJS templates without having to pass
+ * them individually from each route handler
+ */
+app.use((req, res, next) => {
+    // Make NODE_ENV available to all templates
+    res.locals.NODE_ENV = NODE_ENV.toLowerCase() || 'production';
+
+    // Continue to the next middleware or route handler
+    next();
+});
+
 /**
  * Declare Routes
  */
@@ -34,15 +75,21 @@ app.use(express.static(path.join(__dirname, 'public')));
  * Routes
  */
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+    const title = 'Home Page';
+    res.render('home', { title });
+    //res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+    const title = 'About Page';
+    res.render('about', { title });
+    //res.sendFile(path.join(__dirname, 'src/views/about.html'));
 });
 
 app.get('/products', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+    const title = 'Products Page';
+    res.render('products', { title });
+    //  res.sendFile(path.join(__dirname, 'src/views/products.html'));
 });
 
 /**
